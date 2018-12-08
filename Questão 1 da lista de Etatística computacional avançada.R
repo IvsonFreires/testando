9283c512adfeab3efb3b5450074ff7f8c6f12326
IvsonFreires <ivson_freires@hotmail.com>
2018-12-08 11:25:51 -0300

    primeiro exercicio

---
+++ Questão 1 da lista de Etatística computacional avançada.R
@@ -0,0 +1,35 @@
+simula <- function(numero.iteracoes) {
+  n <- numero.iteracoes
+  caixa <- c(rep("m",30),rep("f",70))
+  resultado <- integer(n)
+  for (h in 1:n) {
+    amostra <- sample(caixa, 2)
+    if (amostra[1] == amostra[2])
+      resultado[h] <- 1
+    else
+      resultado[h] <- 0
+  }
+  prob <- mean(resultado)
+  return(prob)
+}
+simula(10000)
+
+#De maneira geral:
+
+simula <- function(numero.iteracoes) {
+  n <- numero.iteracoes
+  caixa <- c(rep("m",macho),rep("f",femea))
+  resultado <- integer(n)
+  for (h in 1:n) {
+    amostra <- sample(caixa, 2)
+    if (amostra[1] == amostra[2])
+      resultado[h] <- 1
+    else
+      resultado[h] <- 0
+  }
+  prob <- mean(resultado)
+  return(prob)
+}
+simula(10000)
+macho=30
+femea=70
